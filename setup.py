527c28013273a15a0f262ef22e52a88cc33fe545
Dan Korostelev <nadako@gmail.com>
2009-03-06 12:53:30 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='zope.password',
-      version='3.5.0dev',
+      version='3.5.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Password encoding and checking utilities',
