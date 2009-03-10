01c4e82780a26a0494302770e9096886c802c499
Dan Korostelev <nadako@gmail.com>
2009-03-10 08:47:51 +0000

    Make security protection directives in `configure.zcml` execute only if ``zope.security`` is installed.
    Move "Password Manager Names" vocabulary here from ``zope.app.authentication``.
    
    Fix changelog formatting.

--- src/zope/password/testing.py
+++ src/zope/password/testing.py
@@ -16,12 +16,16 @@
 $Id$
 """
 __docformat__ = "reStructuredText"
+
 from zope.component import provideUtility
+from zope.schema.interfaces import IVocabularyFactory
+
 from zope.password.interfaces import IPasswordManager
 from zope.password.password import PlainTextPasswordManager
 from zope.password.password import MD5PasswordManager
 from zope.password.password import SHA1PasswordManager
 from zope.password.password import SSHAPasswordManager
+from zope.password.vocabulary import PasswordManagerNamesVocabulary
 
 
 def setUpPasswordManagers():
@@ -29,6 +33,7 @@ def setUpPasswordManagers():
     
     >>> from zope.component import getUtility
     >>> setUpPasswordManagers()
+
     >>> getUtility(IPasswordManager, 'Plain Text')
     <zope.password.password.PlainTextPasswordManager object at 0x...>
     >>> getUtility(IPasswordManager, 'SSHA')
@@ -37,9 +42,25 @@ def setUpPasswordManagers():
     <zope.password.password.MD5PasswordManager object at 0x...>
     >>> getUtility(IPasswordManager, 'SHA1')
     <zope.password.password.SHA1PasswordManager object at 0x...>
+
+    >>> voc = getUtility(IVocabularyFactory, 'Password Manager Names')
+    >>> voc = voc(None)
+    >>> voc
+    <zope.schema.vocabulary.SimpleVocabulary object at 0x...>
+    >>> 'SSHA' in voc
+    True
+    >>> 'Plain Text' in voc
+    True
+    >>> 'SHA1' in voc
+    True
+    >>> 'MD5' in voc
+    True
     
     """
     provideUtility(PlainTextPasswordManager(), IPasswordManager, 'Plain Text')
     provideUtility(SSHAPasswordManager(), IPasswordManager, 'SSHA')
     provideUtility(MD5PasswordManager(), IPasswordManager, 'MD5')
     provideUtility(SHA1PasswordManager(), IPasswordManager, 'SHA1')
+
+    provideUtility(PasswordManagerNamesVocabulary,
+                   IVocabularyFactory, 'Password Manager Names')
