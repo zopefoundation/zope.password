22c68a5e4358edc38b01cf23e782249de89901c9
Stephan Richter <stephan.richter@gmail.com>
2013-03-11 20:21:03 -0400

    Back to development: 4.0.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.password',
-      version='4.0.2',
+      version='4.0.3.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Password encoding and checking utilities',
