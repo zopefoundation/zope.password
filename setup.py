de91bf5b63aec3ab6c3cc603461a858756d7772d
Stephan Richter <stephan.richter@gmail.com>
2013-03-11 20:17:05 -0400

    Preparing release 4.0.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.password',
-      version='4.0.2.dev0',
+      version='4.0.2',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Password encoding and checking utilities',
